fce8d9eea8d9a7dc940156409ba480cd8512eb68
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-02-21 19:59:00 +0530

    Time: 31 ms (39.03%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,14 +5,26 @@ public:
         // sort(nums.begin(),nums.end());
         // return nums[n/2];
         
-        unordered_map<int,int>mp;
-        for(int a:nums)
-            mp[a]++;
-        for(auto b:mp)
+        // unordered_map<int,int>mp;
+        // for(int a:nums)
+        //     mp[a]++;
+        // for(auto b:mp)
+        // {
+        //     if(b.second>n/2)
+        //         return b.first;
+        // }
+        // return 0;
+        
+        
+        int c=0,ele=0;
+        for(int i=0;i<n;i++)
         {
-            if(b.second>n/2)
-                return b.first;
+            if(c==0) ele=nums[i];
+            if(nums[i]==ele)
+                c++;
+            else
+                c--;
         }
-        return 0;
+        return ele;
     }
 };
