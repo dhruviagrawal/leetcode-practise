e6e61ecb482dc526952002e407b94432bb4d483e
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-03-18 00:41:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (83.25%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
+        /*
+        SC O(N)
+        
         int n=s.size();
         stack<int>st;
         int score=0;
@@ -16,6 +19,26 @@ public:
                 score=st.top()+max(2*score,1);
                 st.pop();
             }
+        }
+        return score;
+        */
+        
+        // O(1) space
+        int depth=0,score=0;
+        char prev='(';
+        for(char c:s)
+        {
+            if(c=='(')
+            {
+                depth++;
+            }
+            else
+            {
+                depth--;
+                if(prev=='(')
+                    score+=pow(2,depth);
+            }
+            prev=c;
         }
         return score;
     }
