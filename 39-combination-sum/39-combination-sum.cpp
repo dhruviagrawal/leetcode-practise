eb55df1a653bfbf92bd6a3a344007ca1ed42541b
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-05-20 15:02:40 +0530

    Time: 9 ms (67.01%), Space: 10.8 MB (89.18%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    void findCombination(int index, int target, vector<int>& candidates, vector<int>& ds, vector<vector<int>> &ans)
+    void func(int ind, vector<int>& ds, int sum, vector<int>& candidates, int target, vector<vector<int>>&ans)
     {
         int n=candidates.size();
-        if(index==n)
-        {
-            if(target==0) ans.push_back(ds);
+        if(ind>=n)
             return;
-        }
-        //pick up element
-        if(target>=candidates[index])
-        {
-            ds.push_back(candidates[index]);
-            findCombination(index,target-candidates[index],candidates,ds,ans);
-            ds.pop_back();
-        }
         
-        //don't pick element
-        findCombination(index+1,target,candidates,ds,ans);
+        ds.push_back(candidates[ind]);
+        sum+=candidates[ind];
+        if(sum<target)
+            func(ind, ds, sum, candidates, target, ans);
+        if(sum==target)
+            ans.push_back(ds);
+        sum-=candidates[ind];
+        ds.pop_back();
+        func(ind+1, ds, sum, candidates, target, ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
+        vector<vector<int>>ans;
         vector<int>ds;
-        findCombination(0,target,candidates,ds,ans);
+        func(0, ds, 0, candidates, target, ans);
         return ans;
     }
 };
