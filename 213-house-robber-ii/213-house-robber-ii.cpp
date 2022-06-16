e27571aa2fc19eb2344086f93535dd6e15847da0
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-06-16 17:04:31 +0530

    Time: 4 ms (36.65%), Space: 7.8 MB (60.63%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -27,10 +27,10 @@ public:
         
         if(n==1)
             return nums[0];
-        if(n==2)
-            return max(nums[0],nums[1]);
-        if(n==3)
-            return max(nums[0],max(nums[1],nums[2]));
+        // if(n==2)
+        //     return max(nums[0],nums[1]);
+        // if(n==3)
+        //     return max(nums[0],max(nums[1],nums[2]));
         vector<int>temp1,temp2;
         
         for(int i=0;i<n;i++)
