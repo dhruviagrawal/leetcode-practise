d40d2ccb9801e107f39df5225bd66dcd78b2f6c6
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-06-17 22:35:25 +0530

    Time: 10 ms (50.13%), Space: 9.1 MB (21.25%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int solve(int i,int j, vector<vector<int>>&a, int n, vector<vector<int>>&dp)
-    {
-        if(i==(n-1))
-            return a[i][j];
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int d=a[i][j]+solve(i+1,j,a,n,dp);
-        int dg=a[i][j]+solve(i+1,j+1,a,n,dp);
-        
-        return dp[i][j]=min(d,dg);
-    }
     int minimumTotal(vector<vector<int>>& a) {
         int n=a.size();
         
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        int ans=solve(0,0,a,n,dp);
-        return ans;
+        
+        for(int j=0;j<n;j++)
+            dp[n-1][j]=a[n-1][j];
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int d=a[i][j]+dp[i+1][j];
+                int dg=a[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
