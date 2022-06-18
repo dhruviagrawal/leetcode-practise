bc10bc1506a26066277fae4bd46c4ad2bfa48bff
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-06-18 17:30:24 +0530

    Time: 17 ms (73.77%), Space: 10.3 MB (46.89%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -4,33 +4,35 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<int>prevRow(m,0);
         for(int j=0;j<m;j++)
         {
-            dp[0][j]=matrix[0][j];
+            prevRow[j]=matrix[0][j];
         }
         
         for(int i=1;i<n;i++)
         {
+            vector<int>currRow(m,0);
             for(int j=0;j<m;j++)
             {
                 int s=matrix[i][j];  
-                s+=dp[i-1][j];
+                s+=prevRow[j];
                 int ld=matrix[i][j];
-                if(j-1>=0) ld+=dp[i-1][j-1];
+                if(j-1>=0) ld+=prevRow[j-1];
                 else ld+=1e9;
                 int rd=matrix[i][j];
-                if(j+1<m) rd+=dp[i-1][j+1];
+                if(j+1<m) rd+=prevRow[j+1];
                 else rd+=1e9;
                 
-                dp[i][j]=min(s,min(rd,ld));
+                currRow[j]=min(s,min(rd,ld));
             }
+            prevRow=currRow;
         }
         
         int ans=1e9;
         for(int j=0;j<m;j++)
         {
-            ans=min(ans,dp[n-1][j]);
+            ans=min(ans,prevRow[j]);
         }
         return ans;
     }
