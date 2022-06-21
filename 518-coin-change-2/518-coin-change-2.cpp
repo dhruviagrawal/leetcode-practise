25c18b2a5da81079c154d56afb0d10492e07a4ee
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-06-21 15:53:25 +0530

    Time: 99 ms (13.89%), Space: 18.4 MB (40.14%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int solve(int ind, int t, vector<int>&coins,vector<vector<int>>&dp)
-    {
-        if(ind==0)
-        {
-            if(t%coins[0]==0)
-                return 1;
-            else
-                return 0;
-        }
-        
-        if(dp[ind][t]!=-1)
-            return dp[ind][t];
-        
-        int np=solve(ind-1,t,coins,dp);
-        int p=0;
-        if(coins[ind]<=t)
-            p=solve(ind,t-coins[ind],coins,dp);
-        
-        return dp[ind][t]=p+np;
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,amount,coins,dp);
-        return ans;
+        if(!n)
+            return 0;
+        if(!amount)
+            return 1;
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+
+        for(int t=0;t<=amount;t++)
+        {
+                dp[0][t]=(t%coins[0]==0);
+        }
+        
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int t=0;t<=amount;t++)
+            {
+                int np=dp[ind-1][t];
+                int p=0;
+                if(coins[ind]<=t)
+                    p=dp[ind][t-coins[ind]];
+
+                dp[ind][t]=p+np;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
