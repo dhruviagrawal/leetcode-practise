3d0bc51ab15fcd78d32ea463aa1783b0c3c1467d
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-06-25 19:43:09 +0530

    Time: 37 ms (26.40%), Space: 9.1 MB (20.61%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    int solve(int i, int j, string &w1, string &w2, vector<vector<int>>&dp)
-    {
-        if(i==0) return j;
-        if(j==0) return i;
-        
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        if(w1[i-1]==w2[j-1])
-            return dp[i][j]=solve(i-1,j-1,w1,w2,dp);
-        
-        int ins=1+solve(i,j-1,w1,w2,dp);
-        int dlt=1+solve(i-1,j,w1,w2,dp);
-        int rep=1+solve(i-1,j-1,w1,w2,dp);
-        
-        return dp[i][j]=min(ins, min(dlt,rep));
-    }
     int minDistance(string w1, string w2) {
         int n=w1.size(), m=w2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        int ans=solve(n, m, w1, w2,dp);
-        return ans;
+        
+        for(int j=0;j<=m;j++)
+            dp[0][j]=j;
+        
+        for(int i=0;i<=n;i++)
+            dp[i][0]=i;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(w1[i-1]==w2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                {
+                    int ins=1+dp[i][j-1];
+                    int dlt=1+dp[i-1][j];
+                    int rep=1+dp[i-1][j-1];
+
+                    dp[i][j]=min(ins, min(dlt,rep));
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
