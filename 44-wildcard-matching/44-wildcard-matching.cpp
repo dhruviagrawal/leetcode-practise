7767b35a518ba17693ad3d02aaedd7d2e98610c5
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-06-27 14:19:44 +0530

    Time: 83 ms (53.74%), Space: 6.8 MB (89.19%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    bool f(int i, int j, string &s, string &p, vector<vector<int>>&dp)
-    {
-        if(i==0 && j==0)
-            return true;
-        
-        if(i>0 && j==0)
-            return false;
-        
-        if(i==0 && j>0)
-        {
-            for(int k=1;k<=j;k++)
-                if(p[k-1]!='*')
-                    return false;
-            return true;
-        }
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(s[i-1]==p[j-1] || p[j-1]=='?')
-            return dp[i][j]=f(i-1, j-1, s, p, dp);
-        if(p[j-1]=='*')
-            return dp[i][j]=f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp);
-        
-        return dp[i][j]=false;
-    }
     bool isMatch(string s, string p) {
         int n=s.size(), m=p.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,s,p,dp);
+        vector<bool>prev(m+1,false), curr(m+1,false);
+        prev[0]=true;
+        
+        // for(int i=1;i<=n;i++)
+        // {
+        //     prev[0]=false; //string left, pattern over
+        //     curr[0]=false;
+        // }
+        
+        for(int j=1;j<=m;j++)
+        {
+            //pattern left, string over. true only when all * found
+            bool flag=true;
+            for(int k=1;k<=j;k++)
+            {
+                if(p[k-1]!='*')
+                {
+                    flag=false;
+                    break;
+                }
+            }
+            prev[j]=flag;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1] || p[j-1]=='?')
+                    curr[j]=prev[j-1];
+                else if(p[j-1]=='*')
+                    curr[j]=prev[j] | curr[j-1];
+                else curr[j]=false;
+            }
+            prev=curr;
+        }
+        
+        return prev[m];
     }
 };
