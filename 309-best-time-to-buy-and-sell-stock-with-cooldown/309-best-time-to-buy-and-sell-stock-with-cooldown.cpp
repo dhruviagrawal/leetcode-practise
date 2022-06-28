72abbed1bd7665d5307808d3d32f9710fa8c1690
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-06-28 19:32:27 +0530

    Time: 8 ms (49.95%), Space: 11.9 MB (31.44%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,17 +6,8 @@ public:
         // return f(0, 1, prices, dp);
         for(int i=n-1;i>=0;i--)
         {
-            for(int b=0;b<=1;b++)
-            {
-                if(b)
-                {
-                    dp[i][b]=max(-prices[i]+dp[i+1][0] , dp[i+1][1]);
-                }
-                else
-                {
-                    dp[i][b]=max(prices[i]+dp[i+2][1] , dp[i+1][0]);
-                }
-            }
+           dp[i][1]=max(-prices[i]+dp[i+1][0] , dp[i+1][1]);              
+           dp[i][0]=max(prices[i]+dp[i+2][1] , dp[i+1][0]);       
         }
         
         return dp[0][1];
