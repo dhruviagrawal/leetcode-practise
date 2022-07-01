db11e970f4fed389f53c39e282b34e54944855a3
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-07-01 14:48:58 +0530

    Time: 91 ms (39.89%), Space: 17.7 MB (30.30%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
-    static bool comp(vector<int>&b1, vector<int>&b2)
+    // static bool comp(vector<int>&b1, vector<int>&b2)
+    // {
+    //     return b1[1]>b2[1]; //sorts desc order acc to units per box
+    // }
+    // int maximumUnits(vector<vector<int>>&a, int w) {
+    //     int n=a.size();
+    //     sort(a.begin(), a.end(), comp);
+    //     int ans=0;;
+    //     for(auto it:a)
+    //     {
+    //         if(it[0]<=w)
+    //         {
+    //             w-=it[0];
+    //             ans+=(it[0]*it[1]);
+    //         }
+    //         else
+    //         {
+    //             ans+=(it[1]*w);
+    //             w=0;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    //linear complexity
+    int maximumUnits(vector<vector<int>>&a, int w)
     {
-        return b1[1]>b2[1]; //sorts desc order acc to units per box
-    }
-    int maximumUnits(vector<vector<int>>&a, int w) {
-        int n=a.size();
-        sort(a.begin(), a.end(), comp);
-        int ans=0;;
+        vector<int>sortedboxtypes(1001,0);
         for(auto it:a)
         {
-            if(it[0]<=w)
+            sortedboxtypes[it[1]]+=it[0];
+        }
+        int ans=0;
+        for(int i=1000; i>=0; i--)
+        {
+            if(sortedboxtypes[i]<=w)
             {
-                w-=it[0];
-                ans+=(it[0]*it[1]);
+                w-=sortedboxtypes[i];
+                ans+=(sortedboxtypes[i]*i);
             }
             else
             {
-                ans+=(it[1]*w);
+                ans+=(w*i);
                 w=0;
             }
         }
+        
         return ans;
     }
 };
