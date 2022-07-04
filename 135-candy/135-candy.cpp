640ebfc94a2436110a118034f2536e025d9b3f6f
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-07-04 15:02:27 +0530

    Time: 21 ms (92.11%), Space: 17.8 MB (65.79%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -7,30 +7,6 @@ public:
         
         int ans=0;
         vector<int>c(n,1);
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(i==0 && r[i+1]<r[i])
-        //     {
-        //         c[i]=c[i+1]+1;
-        //     }
-        //     else if(i==n-1 && r[i-1]<r[i])
-        //     {
-        //         c[i]=c[i-1]+1;
-        //     }
-        //     else if(i!=0 && i!=n-1)
-        //     {
-        //         if(r[i]>r[i+1] && r[i]>r[i-1])
-        //             c[i]=max(r[i+1],r[i-1])+1;
-        //         else if(r[i]>r[i-1])
-        //             c[i]=r[i-1]+1;
-        //         else if(r[i]>r[i+1])
-        //             c[i]=r[i+1]+1;
-        //         else 
-        //             continue;
-        //     }
-        //     else 
-        //         continue;
-        // }
         
         for(int i=1;i<n;i++)
         {
