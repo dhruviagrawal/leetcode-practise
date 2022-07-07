f15c5933873e6e4e5b7389a4b478f166e35c1e63
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-07-08 00:47:58 +0530

    Time: 15 ms (60.73%), Space: 8 MB (80.82%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -11,13 +11,18 @@ public:
     bool hasCycle(ListNode *head) {
         if(head==NULL || head->next==NULL)
             return false;
-        ListNode *slow=head, *fast=head;
+        
+        ListNode* slow=head, *fast=head;
+        
         while(fast->next && fast->next->next)
         {
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast) return true;
+            
+            if(slow==fast)
+                return true;
         }
+            
         return false;
     }
 };
