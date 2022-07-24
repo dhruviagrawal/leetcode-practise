c6ea8f4e4b66317aea8efb505915e2e00a87e0af
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-07-24 21:34:34 +0530

    Time: 10 ms (89.01%), Space: 10.8 MB (61.80%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -7,6 +7,19 @@ public:
             return mat;
         vector<vector<int>>ans(r, vector<int>(c,-1));
         
+        
+        /*
+           i/c will give us the row number of output matrix. 
+           We will move to New row after every c elements 
+           and thus dividing by c will give the row number
+
+           i%c will give us the column number of output matrix. 
+           We will be begin from start of new row after every c elements 
+           and this the remainder will give column of current row.
+
+           The same happens in i/n and i%n but for the mat matrix
+        */
+        
         for(int i=0;i<(r*c);i++)
         {
             ans[i/c][i%c]=mat[i/n][i%n];
