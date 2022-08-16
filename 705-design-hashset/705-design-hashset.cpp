f256cadbdfec7d37c88cb7c89d1ff00c3ca9a2f6
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-08-16 12:49:18 +0530

    Time: 146 ms (68.42%), Space: 41.1 MB (75.66%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,22 +1,43 @@
 class MyHashSet {
 public:
-    vector<int>m;
+    vector<list<int>>m;
     int sz;
     MyHashSet() {
-        sz=1e6+1;
+        sz=100;
         m.resize(sz);
     }
     
+    int hash(int key)
+    {
+        return key%sz;
+    }
+    
+    list<int>::iterator search(int key)
+    {
+        int i=hash(key);
+        return find(m[i].begin(), m[i].end(), key);
+    }
+    
     void add(int key) {
-        m[key]=1;
+        if(contains(key))
+            return;
+        
+        int i=hash(key);
+        m[i].push_back(key);
     }
     
     void remove(int key) {
-        m[key]=0;
+        if(!contains(key))
+            return;
+        int i=hash(key);
+        m[i].erase(search(key));
     }
     
     bool contains(int key) {
-        return m[key];
+        int i=hash(key);
+        if(search(key)!=m[i].end())
+            return true;
+        return false;
     }
 };
 
