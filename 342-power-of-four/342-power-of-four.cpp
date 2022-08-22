aa6a7ca2de44a9bfbc7e0c355f404acf0d0109c5
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-08-22 11:19:28 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.37%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        // if(n && (n&(n-1)==0) && (n%3)==1)
+        return (n>0) && ((n&(n-1))==0) && ((n%3)==1);
+            
+        
+        
+        // if(n<1)
+        //     return false;
+        // int m=n;
+        // int c=0;
+        // while(n>1)
+        // {
+        //     n=n>>2;
+        //     c++;
+        // }
+        // while(c--)
+        // {
+        //     n=n<<c;
+        // }
+        // if(m==n)
         //     return true;
-        // return false;
-        if(n<1)
-            return false;
-        int m=n;
-        int c=0;
-        while(n>1)
-        {
-            n=n>>2;
-            c++;
-        }
-        while(c--)
-        {
-            n=n<<2;
-        }
-        if(m==n)
-            return true;
-        else
-            return false;
+        // else
+        //     return false;
         
     }
 };
