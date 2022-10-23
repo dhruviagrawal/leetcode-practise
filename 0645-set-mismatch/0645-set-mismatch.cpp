7dbb12ad4adb96b3aca905fe70390c2c2db042ee
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-10-23 22:17:51 +0530

    Time: 77 ms (54.07%), Space: 21.5 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        unordered_map<int,int>mp;
+        // unordered_map<int,int>mp;
+        // vector<int>ans;
+        // for(int x:nums)
+        // {
+        //     mp[x]++;
+        //     if(mp[x]==2)
+        //         ans.push_back(x);
+        // }
+        // for(int i=1;i<=nums.size();i++)
+        // {
+        //     if(mp.find(i)==mp.end())
+        //     {
+        //         ans.push_back(i);
+        //         break;
+        //     }
+        // }
+        // return ans;
+        
         vector<int>ans;
-        for(int x:nums)
+       
+        for(int i=0;i<nums.size();i++)
         {
-            mp[x]++;
-            if(mp[x]==2)
-                ans.push_back(x);
+            if(nums[abs(nums[i])-1]<0)
+                ans.push_back(abs(nums[i]));
+            else
+                nums[abs(nums[i])-1]*=(-1);
         }
-        for(int i=1;i<=nums.size();i++)
+        
+        for(int i=0;i<nums.size();i++)
         {
-            if(mp.find(i)==mp.end())
+            if(nums[i]>0)
             {
-                ans.push_back(i);
+                ans.push_back(i+1);
                 break;
             }
         }
+        
         return ans;
+        
+       
     }
 };
